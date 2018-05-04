39f869d53b59900e76e957041c2480595b0f3410
MichaelSam <michaelsam08@gmail.com>
2018-05-03 19:10:49 -0500

    first commit

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,9 @@
+'use strict'
+
+
+const = 'printListOfOlympians' = (array1) => {
+  array.forEach(function(item) {
+    console.log(item);
+  })
+
+  printListOfOlympians(['june', 'july', 'august', 'september', 'october']);
