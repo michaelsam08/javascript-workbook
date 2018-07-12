0f796926828d2cd2f30010ce416a105e94d3b8f5
MichaelSam <michaelsam08@gmail.com>
2018-07-12 12:52:43 -0500

    additional comments

--- reactmaterialize/src/App.js
+++ reactmaterialize/src/App.js
@@ -9,19 +9,22 @@ import image4 from './images/sunset1.JPG';
 import image5 from './images/profphoto.jpg';
 import { Document, Page } from 'react-pdf';
 import junDev from './Docs/junDev.pdf'
+//import 3rd party library react materialize
+//imported necessary components and features to render the page as desired
 // added to import my resume in PDF format
 class App extends Component {
   state = {
+//setting state to reflect sunrise / sunset
   sunrise: null,
   sunset: null,
+//setting state to load pdf resume
   numPages: null,
   pageNumber: 1,
 }
 
-  onDocumentLoad = ({ numPages }) => {
-     this.setState({ numPages });
-   }
 
+// after entering Longitude, Latitude and clicking submit it will create a fetch to the api requesting the
+// data specific to those inputs
 handleFormSubmit = (e) => {
   e.preventDefault();
   const lat = e.target.children[1].value;
@@ -39,13 +42,14 @@ handleFormSubmit = (e) => {
 
 }
 
-
+//for pdf of resume to render
   render() {
     const { pageNumber, numPages } = this.state;
-//for pdf
+
 
   return (
       <div className="App">
+
         <Navbar className='green darken-4'>
           <NavItem href= {'#resume'}> Resume</NavItem>
           <NavItem href='components.html'>Components</NavItem>
@@ -61,7 +65,8 @@ handleFormSubmit = (e) => {
         <button type="submit">Submit</button>
         </form>
 
-      <h3 className='yellow-text'>Sunrise and Sunset</h3>
+      <h3 className='yellow-text'><Icon large>loop</Icon>Sunrise and Sunset</h3>
+
         {
           this.state.sunrise ? (
             <div className='Sunrise'>
@@ -71,10 +76,19 @@ handleFormSubmit = (e) => {
           ): null
         }
         <Carousel options={{ fullWidth: true}}
-          fixedItem={<h4 className='white-text'>Michael is a creative and dependable Junior Web Developer. Broadly knowledgeable in React, JavaScript, CSS and HTML as well as the principles and techniques of website construction and maintenance.​ ​An ​effective process-driven student and teacher who creates and implements training for businesses and students. Collaborative team player with the ability to effectively communicate at all organizational and education levels. Exceptional problem solving skills, strong business judgement, and demonstrated experience leading cross-functional teams.
-            </h4>}
-          images={[image1, image2, image3, image4, image5]} >
+          fixedItem={
+            <div>
+              <h1> <Icon medium >keyboard_backspace</Icon>Swipe Left  </h1>
+
+              <h4 className='white-text'>Michael is a creative and dependable Junior Web Developer. Broadly knowledgeable in React, JavaScript, CSS and HTML as well as the principles and techniques of website construction and maintenance.​ ​An ​effective process-driven student and teacher who creates and implements training for businesses and students. Collaborative team player with the ability to effectively communicate at all organizational and education levels. Exceptional problem solving skills, strong business judgement, and demonstrated experience leading cross-functional teams.
+              </h4>
+            </div>
+          }
+            images={[image1, image2, image3, image4, image5]} >
         </Carousel>
+
+        //created form to accept inputs for user's contact information
+        // in the future will connect submit button to forward information to my email
         <div>
           <h3>Request Contact</h3>
           <Row>
@@ -83,6 +97,7 @@ handleFormSubmit = (e) => {
             <button type='submit'>Request Contact</button>
           </Row>
         </div>
+        //
         <div id= 'resume'>
          <Document
            file= {junDev}
@@ -90,9 +105,7 @@ handleFormSubmit = (e) => {
             >
            <Page pageNumber={pageNumber} />
          </Document>
-         <p>Page {pageNumber} of {numPages}</p>
-         <div id='sunrise' className="container"> space for text
-         </div>
+
        </div>
       </div>
 
